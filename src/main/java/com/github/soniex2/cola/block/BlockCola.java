a4b9d747f1d4ed3a32d7dd618ff33d8cb298544e
SoniEx2 <endermoneymod@gmail.com>
2015-04-28 10:48:44 -0300

    Make it work

--- src/main/java/com/github/soniex2/cola/block/BlockCola.java
+++ src/main/java/com/github/soniex2/cola/block/BlockCola.java
@@ -1,6 +1,8 @@
 package com.github.soniex2.cola.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 
@@ -11,5 +13,10 @@ public class BlockCola extends BlockFluidClassic {
     public BlockCola(Fluid fluid, Material material) {
         super(fluid, material);
         this.setBlockName("cola:cola");
+    }
+
+    @Override
+    public boolean isReplaceable(IBlockAccess w, int x, int y, int z) {
+        return isSourceBlock(w, x, y, z);
     }
 }
--- src/main/java/com/github/soniex2/cola/tile/TileColaEngine.java
+++ src/main/java/com/github/soniex2/cola/tile/TileColaEngine.java
@@ -1,13 +1,44 @@
 package com.github.soniex2.cola.tile;
 
+import cofh.api.energy.EnergyStorage;
 import cofh.api.energy.IEnergyProvider;
+import cofh.api.energy.IEnergyReceiver;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.FluidStack;
 
 /**
  * @author soniex2
  */
 public class TileColaEngine extends TileColaBase implements IEnergyProvider {
+
+    private EnergyStorage storage = new EnergyStorage(80000, 10, 1000) /*{
+        @Override
+        public String toString() {
+            return energy + " " + capacity + " " + maxExtract + " " + maxReceive;
+        }
+    }*/;
+
+    @Override
+    public void updateEntity() {
+        super.updateEntity();
+        if (worldObj.isRemote) return;
+        //System.out.println(storage + " " + (colaTank.getFluid() == null ? "null" : colaTank.getFluid().getFluid().getName()) + " " + colaTank.getFluidAmount() + " " + colaTank.getCapacity());
+        if (storage.receiveEnergy(10, true) == 10) {
+            FluidStack fs = colaTank.drain(1, true);
+            if (fs != null && fs.amount == 1) {
+                storage.receiveEnergy(10, false);
+            }
+        }
+        for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
+            TileEntity te = worldObj.getTileEntity(this.xCoord + dir.offsetX, this.yCoord + dir.offsetY, this.zCoord + dir.offsetZ);
+            if (te instanceof IEnergyReceiver) {
+                storage.extractEnergy(((IEnergyReceiver) te).receiveEnergy(dir.getOpposite(), storage.extractEnergy(10, true), false), false);
+            }
+        }
+    }
+
     /**
      * Returns TRUE if the TileEntity can connect on a given side.
      *
@@ -15,7 +46,7 @@ public class TileColaEngine extends TileColaBase implements IEnergyProvider {
      */
     @Override
     public boolean canConnectEnergy(ForgeDirection from) {
-        return false;
+        return true;
     }
 
     /**
@@ -28,7 +59,7 @@ public class TileColaEngine extends TileColaBase implements IEnergyProvider {
      */
     @Override
     public int extractEnergy(ForgeDirection from, int maxExtract, boolean simulate) {
-        return 0;
+        return storage.extractEnergy(maxExtract, simulate);
     }
 
     /**
@@ -38,7 +69,7 @@ public class TileColaEngine extends TileColaBase implements IEnergyProvider {
      */
     @Override
     public int getEnergyStored(ForgeDirection from) {
-        return 0;
+        return storage.getEnergyStored();
     }
 
     /**
@@ -48,6 +79,18 @@ public class TileColaEngine extends TileColaBase implements IEnergyProvider {
      */
     @Override
     public int getMaxEnergyStored(ForgeDirection from) {
-        return 0;
+        return storage.getMaxEnergyStored();
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound tag) {
+        super.readFromNBT(tag);
+        storage.readFromNBT(tag);
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound tag) {
+        super.writeToNBT(tag);
+        storage.writeToNBT(tag);
     }
 }
